a05fc2142fcfc0e809b12ceb68b537d22f68090a
khanhhung0810 <lhkhungg@gmail.com>
2024-04-16 15:05:19 +0700

    push dự án cá nhân

---
+++ duancanhan/src/test/java/khanhhung/duan/duancanhan/DuancanhanApplicationTests.java
@@ -0,0 +1,13 @@
+package khanhhung.duan.duancanhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuancanhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
