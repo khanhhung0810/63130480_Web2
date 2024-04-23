1451a236eb6c70c1d6e9962e5085f10bd91cd6ef
khanhhung0810 <lhkhungg@gmail.com>
2024-04-23 14:17:14 +0700

    add-template, fragments

--- duancanhan/src/main/java/khanhhung/duan/duancanhan/controller/HomeController.java
+++ duancanhan/src/main/java/khanhhung/duan/duancanhan/controller/HomeController.java
@@ -1,5 +1,18 @@
 package khanhhung.duan.duancanhan.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    
+    @GetMapping("/")
+    public String index(){
+        return "home/index";
+    }
+    @GetMapping("/shop")
+    public String shop(){
+        return "home/shop";
+    }
 
 }
